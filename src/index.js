078fd476ef11f746d426411dd63dd9d6f57086ed
LehlohonoloMofula <LehlohonoloHarrison>
2023-08-19 01:38:14 +0200

    h

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@
 navigator.serviceWorker.register(new URL("service-worker.js", import.meta.url), { type: "module" });
 import React from "react";
 import { render } from "react-dom";
-
 import App from "./App";
-
 render(<App></App>, document.getElementById("app"));
